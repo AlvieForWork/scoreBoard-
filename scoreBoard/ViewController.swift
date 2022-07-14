5227e304c6d89681fed11f629e3df6449191bc72
Alvie <alvieforwork@gmail.com>
2022-07-14 19:36:17 +0800

    Initial Commit

---
+++ scoreBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scoreBoard
+//
+//  Created by worker on 2022/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
